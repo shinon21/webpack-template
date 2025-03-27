7d6daaec27bcf7d14a4585594eb3cf1c07d7cd9d
Shinon <204764867+shinon21@users.noreply.github.com>
2025-03-27 14:55:17 -0500

    Create basic configuration

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(png|jpg|svg|jpeg|webp|gif|avif)$/i,
+                type: "asset/resource"
+            },
+            {
+                test: /\.ttf$/i,
+                type: "asset/resource"
+            }
+        ]
+    },
+    devtool: "eval-source-map",
+    devServer: {
+        watchFiles: ["./src/template.html"]
+    },
+
+}
